86fcc59b092614d64eb2b0299c148ad3deeadfd4
danacinque <danacinque@104.39.93.4>
2014-09-23 15:41:25 -0400

    I have no idea what I'm doing

---
+++ src/myJPanel.java
@@ -0,0 +1,87 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class myJPanel extends JPanel implements ActionListener
+{
+
+    JButton score;
+    JButton clickMe;
+    JButton start;
+    Timer timer;
+    Point clickMeLocation;
+    int timerDelay;
+    Integer currentScore;
+    
+    public myJPanel()
+    {
+        super();
+        
+        start = new JButton("Start");
+        start.addActionListener(this);
+        add(start);
+        
+        clickMe = new JButton("Click Me");
+        clickMe.addActionListener(this);
+        add(clickMe);
+        clickMe.setVisible(false);
+        
+        currentScore = 0;
+        score = new JButton("Score");
+        add(score);
+        
+        timerDelay = 2000;
+        timer = new Timer(timerDelay, this);
+        
+        
+    }
+    
+    public void updateScore()
+    {
+        Integer currentScore = Integer.parseInt(score.getText());
+        currentScore++;
+        score.setText(currentScore.toString());
+    }
+    
+    public int updateDelay()
+    {
+        timerDelay -= 500;
+        return timerDelay;
+    }
+    
+    public void actionPerformed(ActionEvent event) throws IllegalArgumentException
+    {
+        Object obj = event.getSource();
+        
+        
+        if (obj == start)
+        {
+            clickMe.setVisible(true);
+            score.setText("0");
+            timer.start();
+            start.setVisible(false);
+        }
+
+        if (obj == timer)
+        {
+            int newX = (int) (Math.random() * this.getWidth());
+            int newY = (int) (Math.random() * this.getHeight());
+            clickMe.setLocation(newX, newY);
+        }
+        
+        if (obj == clickMe)
+        {
+            try 
+            {
+                updateScore();
+                timer.setDelay(updateDelay());
+            }
+            catch (Exception e)
+            {
+                System.out.println("Delay has reached 0");
+            }
+        }
+        
+    }
+}
